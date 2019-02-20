32c09a6b70d38b1d715550404481b7dd4ed17d9f
Fortune Ekeruo <legalmody@gmail.com>
2019-02-20 09:22:24 +0100

    implement create functionality and show functionality

--- index.js
+++ index.js
@@ -1,73 +1,11 @@
-import { exec } from 'child_process';
+import {
+    exec
+} from 'child_process';
 import readline from 'readline';
 
+exec('mkdir output', (err, stdout, stderr) => { stderr });
+
 const rl = readline.createInterface(process.stdin, process.stdout);
-let run = runApp();
-
-// rl.setPrompt('App started >>>> \n');
-// rl.prompt();
-
-// rl.write('This a simple console file management app');
-
-// exec("ls", (err, stdout, stderr) => {
-//     if (err) {
-//         console.log(stderr.split(`${err.cmd}:`)[1]);
-//         return;
-//     }
-//     console.log(stdout);
-// });
-
-function runApp () {
-
-    rl.question(`\n enter command or type help to see available commands: `, (cmd) => {
-  
-      let command = cmd.toLowerCase();
-
-      switch(command) {
-        case 'help':
-            console.log('pulling up some help for you one sec...');
-            help()
-            break;
-        case 'create':
-            console.log('create operation underway...');
-            create();
-            break;
-        case 'delete':
-            console.log('delete operation underway');
-            del();
-            break;
-        case 'rename':
-            console.log('rename op');
-            break;
-        case 'q':
-            console.log('bye for now...');
-            process.exit()
-            break;
-        default:
-            console.log('Invalid command');
-      }
-  
-    //   if (command === "exit") {
-    //     rl.write(`Application exited.`);
-    //     process.exit();
-    //   }
-  
-    //   rl.write(`Getting definition for 'word' ${word}` + `\n`);
-  
-    //   getWordDefinition(word)
-    //     .then(function(reponse){
-    //       displayDefintion(reponse.data, function() {
-    //         runApp();
-    //       });
-  
-    //     })
-    //     .catch(function (response) {
-    //       wordDefinitionError(response);
-    //       runApp();
-    //     });
-    });
-  
-  }
 
 function help() {
     rl.write(
@@ -77,6 +15,9 @@ function help() {
                 - parameters includes --file <file name> and --folder <folder name> 
                 - eg. create --file text.txt
                 - eg. create --folder newFolder
+
+            show:
+                - displays output directory content
 
             rename: 
                 - renames a file or dir depending on specified parameter
@@ -88,7 +29,7 @@ function help() {
             delete: 
                 - deletes a file or dir depending on specified parameter
                 - parameters includes --file <file name> and --folder <folder name> 
-                - eg. delete --file text.txt
+                - eg. delete --file text.txt (specify an extension to the file name)
                 - eg. delete --folder newFolder
 
             q:
@@ -98,51 +39,83 @@ function help() {
                 - basically displays help/guide on how to use
         `
     )
+    runApp();
 }
 
 
-function create() {
-
-    return `created`
+function create(flag, [name = 'unnamed']) {
+    if (flag === '--file') {
+        exec(`touch output/${name}`, (err, stdout, stderr) => {
+            if (err || stderr) {
+                console.log(err, stderr);
+            }
+            console.log(stdout);
+            runApp();
+        });
+    } else if (flag === '--folder') {
+        exec(`mkdir output/${name}`, (err, stdout, stderr) => {
+            if (err || stderr) {
+                console.log(err, stderr);
+            }
+            console.log(stdout);
+            runApp();
+        });
+    } else {
+        console.log('Invalid flag, please specify a file or folder flag. See  help');
+        runApp();
+    }
 }
 
-function rename() {
-    
-    return `renamed`
+function showDirectory() {
+    exec('ls output', (err, stdout, stderr) => {
+        if (err) {
+            console.log('Error:', stderr.split(`${err.cmd}:`)[1]);
+        }
+        console.log(stdout);
+        runApp();
+    });
 }
 
 function del() {
 
-    return `deleted`
+    console.log(`deleted`);
+    runApp();
 }
-  
-//   function getWordDefinition(word) {
-//       return axios.get(`http://api.wordnik.com:80/v4/word.json/${word}/definitions`, {
-//       params: {
-//         limit: 200,
-//         includeRelated: true,
-//         useCanonical: false,
-//         includeTags: false,
-//         api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
-//       }
-//     })
-//   }
-  
-//   function wordDefinitionError(response) {
-//     rl.write(`Error getting word defintion pleasee check word spelling.`);
-//   }
-  
-//   function displayDefintion(arrayValue, cb) {
-//     var numberFound = arrayValue.length;
-//     rl.write(`Found ${numberFound} result(s): ` + `\n`);
-  
-//     arrayValue.forEach(function(item, idx) {
-//       rl.write(`${idx + 1}` + `\n`);
-//       rl.write(`Part of Speech:  ${item.partOfSpeech}` + `\n`);
-//       rl.write(`Definition: ${item.text}` + `\n` + `\t`);
-//     });
-  
-//     cb();
-//   }
-  
-// run  = runApp();
+
+
+function runApp() {
+    rl.question(`enter command or type help to see available commands: `, (input) => {
+
+        let cmd = input.toLowerCase().split(' ');
+        let command = cmd[0];
+        let flag = cmd[1] ? cmd[1] : '';
+        let resourceNames = [cmd[2], cmd[3]];
+
+        switch (command) {
+            case 'help':
+                console.log('pulling up some help for you one sec...');
+                help()
+                break;
+            case 'create':
+                create(flag, resourceNames);
+                break;
+            case 'show':
+                showDirectory()
+                break;
+            case 'delete':
+                console.log('delete operation underway');
+                del();
+                break;
+            case 'q':
+                console.log('bye for now...');
+                process.exit()
+                break;
+            default:
+                console.log('Invalid command');
+                runApp()
+        }
+    });
+
+}
+
+let run = runApp();
